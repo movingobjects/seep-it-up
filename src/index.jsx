fede6f0d9e79b97d0146d5542868ab746b25eed2
Scott Thiessen <scott@movingobjects.io>
2026-09-17 15:30:46 -0700

    Use movingobjects eslint dependency

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
+import App from '@components/App/index.jsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '@components/App/index.jsx';
 import '@styles/reset.scss';
 
 createRoot(document.getElementById('root')).render(
